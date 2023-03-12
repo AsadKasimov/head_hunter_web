a64f7edb554c91de764fe24a37d31cc0284658f8
GithubRedMouth <90645738+GithubRedMouth@users.noreply.github.com>
2023-03-12 07:46:51 +0300

    добавил allure

--- hh_ru/model/pages/search_for_employer.py
+++ hh_ru/model/pages/search_for_employer.py
@@ -1,16 +1,21 @@
+import allure
 from selene.support.conditions import have
 from selene.support.shared import browser
 from hh_ru.utils.scroll import scroll_to_
 
 
 def open_for_employer():
-    browser.open('/employer')
+    with allure.step(f'Открыть в роли Работодателя'):
+        browser.open('/employer')
 
 def scroll_to_search_zone():
-    scroll_to_('[data-qa="employer-index-search-input"]')
+    with allure.step(f'Скроллинг до поискового поля'):
+        scroll_to_('[data-qa="employer-index-search-input"]')
 
 def set_searh_resume(text: str):
-    browser.element('[data-qa="employer-index-search-input"]').type(text).press_enter()
+    with allure.step(f'Ввод {text} в поле'):
+        browser.element('[data-qa="employer-index-search-input"]').type(text).press_enter()
 
 def assert_search_size_greater(number):
-    browser.all('[data-qa="resume-serp__resume"]').should(have.size_greater_than(number))
+    with allure.step(f'Проверка, нашлось резюме больше чем {number}'):
+        browser.all('[data-qa="resume-serp__resume"]').should(have.size_greater_than(number))
